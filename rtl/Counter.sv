534d91cbd0c035829f87ab2ecbc8d01a03282f24
Aakash <aakashnareshgupta@gmail.com>
2024-04-06 16:32:14 +0530

    Very basic Conter module with no reset logic
    The Counter size is paramererizable. This can be used as
    a clock cycle counter, so no reset is required.

---
+++ rtl/Counter.sv
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+
+module Counter #(parameter WIDTH = 8) (
+    input logic                 clock,
+    output logic [WIDTH-1:0]    count = '0
+);
+
+    always_ff @(posedge clock) begin
+        count <= count + 1;
+    end
+
+endmodule
---
+++ test/Counter_tb.sv
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+
+module Counter_tb;
+
+    // Parameters
+    parameter WIDTH = 8;
+    int errors = 0;
+    int cycles = 0;
+
+    // Outputs
+    logic [WIDTH-1:0]    count;
+
+    // Clock generation
+    logic clk = 0;
+    always #5 clk = ~clk;
+
+    // Instantiate Adder module
+    Counter #(WIDTH) DUT (
+        .clock(clk),
+        .count(count)
+    );
+
+
+    // Test stimulus
+    initial begin
+
+        // Dump VDC file
+        $dumpfile("output/Counter.vcd");
+        $dumpvars(0, Counter_tb);
+
+        $display("Counter value: %d", count);
+
+        #10000
+
+        // End simulation
+        $finish;
+    end
+
+endmodule
+
+/***l
+Notes
+
+when rs1 is WIDTH/2, say 8-bits and $signed is used, would it sign extend the rs1 value to required bits when reading and comparing?
+*/
