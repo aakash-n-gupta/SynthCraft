dad67c71117d258d3036e254dfa87dd9c17b3d1e
Aakash <aakashnareshgupta@gmail.com>
2024-04-09 15:05:16 +0530

    FIFO module with testbench.
    - FIFO keeps track of number of items with counter
    - Testbench is needs more flare, some things not working as expected - see FIFO.md
    - Added gtkw file to make it easier to reload vcd file

--- rtl/FIFO.sv
+++ rtl/FIFO.sv
@@ -1,57 +1,88 @@
-`timescale 1ns / 1ps
+`timescale 1ns / 1ns
 
 module FIFO #(parameter int DEPTH = 1024) (
     input clock,
     input resetn,
     input wr_en,
     input rd_en,
-    input logic [31:0] data_in,
+    input [31:0] data_in,
 
     output full,
     output empty,
     output logic [31:0] data);
 
     logic [31:0] fifo [DEPTH];
+    logic [31:0] data_read_out;
 
     // Keep track of fifo
     logic [($clog2(DEPTH) - 1):0] rd_ptr;
     logic [($clog2(DEPTH) - 1):0] wr_ptr;
+    logic [($clog2(DEPTH) - 1):0] counter;
 
-    // if wr_ptr = 1024, fifo full, if 0, fifo empty
-    assign full     = (wr_ptr == DEPTH) ? '1 : '0;
-    assign empty    = (wr_ptr == '0) ? '1 : '0;
-
-    // move data_in into FIFO current wr_ptr,
+    // Keep track of counter
     always_ff @(posedge clock)
     begin
         if (!resetn) begin
-            // flush all fifo data??
-            data <= '0;
-            wr_ptr <= '0;
+            // FIFO will be reset, data will not be available
+            counter <= '0;
             rd_ptr <= '0;
-
-        end
-
-        else
-        begin
-            if (wr_en && !full)
-                fifo[wr_ptr] <= data_in;
-        end
-    end
-
-    always_ff @(posedge clock)
-    begin
-        // write 0 to FIFO during reset
-        if (!resetn) begin
-            fifo[rd_ptr] <= '0;
+            wr_ptr <= '0;
         end
         else begin
-            if (rd_en && !empty) begin
-                data <= fifo[rd_ptr];
-                rd_ptr <= rd_ptr + 1;
+            case ({rd_en, wr_en})
+            2'b00: begin
+                counter <= counter;
+                wr_ptr <= wr_ptr;
             end
+
+            2'b01: begin
+                if (!full) begin
+                    counter <= counter + 1;
+                    wr_ptr <= wr_ptr + 1;
+                end
+            end
+
+            2'b10: begin
+                if(!empty) begin
+                    counter <= counter - 1;
+                    wr_ptr <= wr_ptr - 1;
+                end
+            end
+
+            2'b11: begin
+                counter <= counter;
+                wr_ptr <= wr_ptr;
+            end
+            endcase
         end
     end
 
+
+
+    //  Push data into FIFO @wr_ptr
+    always_ff @(posedge clock)
+    begin
+        if (!full && wr_en) begin
+            fifo[wr_ptr] <= data_in;
+        end
+    end
+
+    // Pop data from FIFO head
+    always_ff @(posedge clock)
+    begin
+        rd_ptr <= '0;
+        data_read_out <= '0;
+        if (!empty && rd_en) begin
+            data_read_out <= fifo[rd_ptr];
+            // fifo[rd_ptr] <= fifo[rd_ptr + 1];
+            rd_ptr <= rd_ptr + 1;
+        end
+    end
+
+    // if wr_ptr = 1024, fifo full, if 0, fifo empty
+    assign full     = (counter == '1);
+    assign empty    = (counter == '0);
+
+    assign data = data_read_out;
 
 endmodule
--- test/FIFO_tb.sv
+++ test/FIFO_tb.sv
@@ -1,18 +1,21 @@
-`timescale 1ns / 1ps
+`timescale 1ns / 1ns
 
 module FIFO_tb;
-    parameter DATA_WIDTH = 32;
 
-    reg clock, resetn;
-    reg wr_en, rd_en;
-    reg [DATA_WIDTH-1:0] data_in;
-    wire [DATA_WIDTH-1:0] data_out;
-    wire full, empty;
-  
+  parameter DATA_WIDTH = 32;
+  parameter MAX = 1024;
+
+  logic clock, resetn;
+  logic wr_en, rd_en;
+  logic [DATA_WIDTH-1:0] data_in;
+  logic [DATA_WIDTH-1:0] data_out;
+  logic full, empty;
+
   // Queue to push data_in
-  reg [DATA_WIDTH-1:0] wdata_q[$], wdata;
+  logic [DATA_WIDTH-1:0] wdata_q[$], wdata;
 
-  FIFO s_fifo ( clock,
+  FIFO #(.DEPTH(1024)) s_fifo
+                (clock,
                 resetn,
                 wr_en,
                 rd_en,
@@ -25,54 +28,99 @@ module FIFO_tb;
     always #5ns clock = ~clock;
 
     initial begin
-        clock = 1'b0;
-        resetn = 1'b0;
-        wr_en = 1'b0;
-        data_in = 0;
-
-        repeat(10) @(posedge clock);
-            resetn = 1'b1;
-
-    repeat(2) begin
-        for (int i=0; i<30; i++) begin
-            @(posedge clock);
-                wr_en = (i%2 == 0)? 1'b1 : 1'b0;
-                if (wr_en & !full) begin
-                    data_in = $urandom;
-                    wdata_q.push_back(data_in);
-                end
-            end
-      #50;
-    end
+      clock = 1'b0;
+      resetn = 1'b0;
+      wr_en = 1'b0;
+      rd_en = '0;
+      data_in = '0;
+      wdata = '0;
   end
 
   initial begin
-    clock = 1'b0;
-    resetn = 1'b0;
-    rd_en = 1'b0;
-
-    repeat(20) @(posedge clock);
-        resetn = 1'b1;
-
-    repeat(2) begin
-      for (int i=0; i<30; i++) begin
-        @(posedge clock);
-        rd_en = (i%2 == 0)? 1'b1 : 1'b0;
-        if (rd_en & !empty) begin
-          #1;
-          wdata = wdata_q.pop_front();
-          if(data_out !== wdata) $error("Time = %0t: Comparison Failed: expected wr_data = %h, rd_data = %h", $time, wdata, data_out);
-          else $display("Time = %0t: Comparison Passed: wr_data = %h and rd_data = %h",$time, wdata, data_out);
-        end
-      end
-      #50;
-    end
-
-    $finish;
-  end
-  
-  initial begin 
     $dumpfile("output/FIFO.vcd");
     $dumpvars(0, FIFO_tb);
   end
+
+  task push(int MAX);
+    if(!full) begin
+      for (int i = 0; i < MAX; ++i) begin
+        wr_en = 1;
+        rd_en = 0;
+        data_in = $urandom;
+        wdata_q.push_back(data_in);
+        #10;
+        $display("Push In: wr_en = %d, rd_en = %d, data_in = %d", wr_en, rd_en, data_in);
+      end
+    end
+    else if(full)
+      $display("FIFO Full!! Can not push data_in = %d ", data_in);
+  endtask
+
+
+  task pop(int MAX);
+    if(!empty) begin
+      for (int i = 0; i < MAX; ++i) begin
+        rd_en = 1;
+        wr_en = 0;
+        #10;
+        if (data_out != wdata_q.pop_front()) begin
+          $display("Test Failed, wdata = %d data_out = %d", wdata_q.pop_front(), data_out);
+        end
+        $display("Pop Out: wr_en = %d, rd_en = %d, data_out = %d", wr_en, rd_en, data_out);
+      end
+    end
+    else if (empty)
+      $display("FIFO Empty!! Can not pop data_out");
+  endtask
+
+  initial begin
+    #10;
+    resetn = '1;
+    #10;
+    $display("Push 24 data in");
+    push(24);
+    $display("Pop 20 data out");
+    pop(20);
+    $display("Pop 4 data out");
+    pop(4);
+    rd_en = '0;
+    wr_en  = '0;
+    data_in = '0;
+    #100;
+    $display("--------------------------------");
+    $display("TEST1: Fill fifo to 127");
+    push(1023);
+    $display("Pushed 127 words into FIFO");
+    $display("Starting to read data from fifo");
+    pop(1023);
+    $display("--------------------------------");
+    $display("TEST1: Fill fifo to 127 Complete");
+
+
+    // Test reset\
+    $display("TEST2: TEST RESET");
+    resetn = '0;
+    rd_en = '0;
+    #50;
+    resetn = '1;
+    #100;
+    $display("FIFO State|| Full = %d  Empty = %d", full, empty);
+    $display("");
+    $display("--------------------------------");
+    $display("TEST3: fifo Overflow");
+    $display("Push 1030 words into FIFO");
+    push(1030);
+    // $display("Overflow FIFO");
+    $display("Starting to read data from fifo, after overflow");
+    pop(1023);
+    $display("--------------------------------");
+    $display("TEST3: fifo Overflow completed");
+
+    // push(1024);
+    // pop(1);
+    // push(98);
+    #10
+    $finish;
+  end
+
 endmodule
